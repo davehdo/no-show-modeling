406eaf67468d80c32dfaa1b4d270b151aa954109
David Do <davehdo@gmail.com>
2017-06-04 19:33:26 -0400

    using random forest via weka library, switched to jruby

--- main.rb
+++ main.rb
@@ -2,8 +2,14 @@
 # Report is calling “Charges, Payments and Adjustments”
 # exported from business objects… (PROMIS)
 
-# use ruby 2.2.1
-recommend_ruby_version = "2.2.1"
+# =====================   GETTING STARTED INSTRUCTIONS   ====================
+# use jruby-9.1.2.0 rather than ruby
+# rbenv local jruby-9.1.2.0
+# which has equivalence of ruby 2.3.0
+
+# first run gem install weka (but first ensure that jruby is chosen)
+
+recommend_ruby_version = "2.3.0"
 
 puts "Warning: Running ruby version #{  RUBY_VERSION }. (Recommend #{ recommend_ruby_version })" unless recommend_ruby_version ==  RUBY_VERSION
 
@@ -14,35 +20,16 @@ require "./analyze.rb"
 # require 'yaml'
 
 
-begin
-  require 'statsample'
-rescue
-  puts "cannot find statssample; run gem install statsample"
-end
+# begin
+#   require 'statsample'
+# rescue
+#   puts "cannot find statssample; run gem install statsample"
+# end
 
-# raise (1..100).each {|e| puts [e, Analyze.categorize_continuous_variable( e, 2, 4, 4 )].join(" -> ")}.inspect
+require 'weka' # requires jruby
 
-# include Statsample
-
-# scatter plot example
-# a=[1,2,3,4].to_vector
-# b=[3,4,5,6].to_vector
-# rb=ReportBuilder.new
-# rb.add(Statsample::Graph::Scatterplot.new(a,b))
-# rb.save_html('scatter.html')
-
-# multiple regression example
-# a=1000.times.collect {rand}.to_vector(:scale)
-# b=1000.times.collect {rand}.to_vector(:scale)
-# c=1000.times.collect {rand}.to_vector(:scale)
-#
-# ds={'a'=>a,'b'=>b,'c'=>c}.to_dataset
-# puts a
-# # ds['y']=ds.collect{|row| row['a'] * 5  +  row['b'] * 3  +  row['c'] * 2  +  rand()}
-#
-# lr=Statsample::Regression.multiple(ds,'c')
-# puts lr.summary
-
+# puts Weka::Classifiers::Trees::RandomForest.options
+# puts Weka::Classifiers::Trees::RandomForest.description
 
 
 # =============================================================================
@@ -157,25 +144,23 @@ encounters_by_mrn = @encounters_all.group_by {|e| e["MRN"]}
   e["features"] = {
     "dist_#{ Analyze.categorize_continuous_variable_log(dist, 2, 4, 0, 1024) }_km" => 1,
     "age_decade_#{ Analyze.categorize_continuous_variable( e["Age at Encounter"].to_i, 10, 3, 10, 90)  }" => 1,
-    "zip_#{ zip }" => 1,
+    # "zip_#{ zip }" => 1,
     "gender_male" => e["Gender"].downcase == "male" ? 1 : 0,
     "appt_made_#{ Analyze.categorize_continuous_variable_log(e["appt_booked_on"] ? (e["appt_at"] - e["appt_booked_on"] + 1) : nil, 2, 4, 0, 256 )  }d_advance" => 1,
     # "dept_#{ e["Department"].downcase.gsub(" ", "_") }" => 1,
     "appt_hour_#{ e["appt_at"].hour.to_s.rjust(2, "0") }" => 1,
     "appt_type_#{ e["Visit Type"].downcase }" => 1,
-    "prior_show_past_2yr" =>  n_prior_encounters_show,
-    "prior_no_show_past_2yr" => n_prior_encounters_no_show,
-    "prior_cancellations_past_2yr" => n_prior_encounters_cancelled
-    # "prior_show_past_2yr_#{ n_prior_encounters_show.to_s.rjust(2, "0") }" => 1,
-    # "prior_no_show_past_2yr_#{ n_prior_encounters_no_show.to_s.rjust(2, "0") }" => 1,
-    # "prior_cancellations_past_2yr_#{ n_prior_encounters_cancelled.to_s.rjust(2, "0") }" => 1
+    # "prior_show_past_2yr" =>  n_prior_encounters_show,
+    # "prior_no_show_past_2yr" => n_prior_encounters_no_show,
+    # "prior_cancellations_past_2yr" => n_prior_encounters_cancelled,
+    "prior_show_past_2yr_#{ n_prior_encounters_show.to_s.rjust(2, "0") }" => 1,
+    "prior_no_show_past_2yr_#{ n_prior_encounters_no_show.to_s.rjust(2, "0") }" => 1,
+    "prior_cancellations_past_2yr_#{ n_prior_encounters_cancelled.to_s.rjust(2, "0") }" => 1,
+    "no_show" => [e].status_no_show.any? ? true : ([e].status_completed.any? ? false : nil)
   }.select {|k,v| v!=nil }
 
   e["features"].delete("dist__km")
 
-  e["outcomes"] = {
-    "no_show" => [e].status_no_show.any? ? true : ([e].status_completed.any? ? false : nil)
-  }
   
 }
 puts "  done"
@@ -187,85 +172,166 @@ puts "  done"
 
 puts "Separating into training and test sets"
 
-training_fraction = 0.45
-all_show = @encounters_all.status_completed
-all_no_show = @encounters_all.status_no_show
+training_fraction = 0.4
+# all_show = @encounters_all.status_completed
+# all_no_show = @encounters_all.status_no_show
+#
+# training_show = all_show.sample( (all_show.size * training_fraction).round)
+# training_no_show = all_no_show.sample( (all_no_show.size * training_fraction).round)
+# test_show = all_show - training_show
+# test_no_show = all_no_show - training_no_show
 
-training_show = all_show.sample( (all_show.size * training_fraction).round)
-training_no_show = all_no_show.sample( (all_no_show.size * training_fraction).round)
-test_show = all_show - training_show
-test_no_show = all_no_show - training_no_show
+all = @encounters_all.status_completed + @encounters_all.status_no_show
+
+training_set = all.sample( (all.size * training_fraction).round)
+test_set = all - training_set
 
 
 # ======================   method 1
 
-lr_model = Analyze.train_multiple_regression( training_no_show, training_show )
+# lr_model = Analyze.train_multiple_regression( training_no_show, training_show )
 
-puts lr_model.summary
+# puts lr_model.summary
 
 # ======================   method 2
 #
-feature_statistics_array = Analyze.train_odds_ratios( training_no_show, training_show )
-  
-
-headers = feature_statistics_array[0].keys
-
-puts "Saving as --#{ stats_odds_ratios_filename }--"
-CSV.open("#{ stats_odds_ratios_filename }", "wb") do |csv|
-  csv << headers
-  feature_statistics_array.each do |items|
-    csv << items.values
-  end
-end
+# feature_statistics_array = Analyze.train_odds_ratios( training_no_show, training_show )
 #
-log_odds_ratios_by_feature_2 = Hash[
-  feature_statistics_array
-    .select {|e| e[:or_80_ci_lower] > 1 or e[:or_80_ci_upper] < 1}
-    .collect {|e| [e[:feature_name], e[:log_odds_ratio]] }
-]
+#
+# headers = feature_statistics_array[0].keys
+#
+# puts "Saving as --#{ stats_odds_ratios_filename }--"
+# CSV.open("#{ stats_odds_ratios_filename }", "wb") do |csv|
+#   csv << headers
+#   feature_statistics_array.each do |items|
+#     csv << items.values
+#   end
+# end
+# #
+# log_odds_ratios_by_feature_2 = Hash[
+#   feature_statistics_array
+#     .select {|e| e[:or_80_ci_lower] > 1 or e[:or_80_ci_upper] < 1}
+#     .collect {|e| [e[:feature_name], e[:log_odds_ratio]] }
+# ]
+#
+
+features = @encounters_all.collect {|e| e["features"].to_a}.flatten(1).group_by {|k,v| k}
+
+unique_feature_names = features.keys 
 
 
 
+def get_instances_from_encounters( encounters, feature_names)
+
+  # create instances with relation name 'weather' and attributes
+  instances = Weka::Core::Instances.new(relation_name: 'encounter').with_attributes do
+    feature_names.each do |feature_name|
+      nominal feature_name, values: [true, false]
+    end
+  
+    # nominal :gender_male, values: [true, false]
+    # nominal :no_show, values: [true, false]
+    # numeric :temperature
+    # numeric :humidity
+    # nominal :windy, values: [true, false]
+    # date    :last_storm, 'yyyy-MM-dd'
+    # nominal :play, values: [:yes, :no], class_attribute: true
+  end
+  
+  data = encounters.collect do |encounter|
+    feature_names.collect {|feature_name| encounter["features"][feature_name] == 1 or encounter["features"][feature_name] == true}
+  end
+
+  instances.add_instances(data) # , weight: 2.0
+  instances
+end
+
+puts "Assembling the training and test instances"
+training_instances = get_instances_from_encounters( training_set, unique_feature_names)
+training_instances.class_attribute = :no_show
+training_instances.to_csv('encounters_training.csv')
+
+test_instances = get_instances_from_encounters( test_set, unique_feature_names)
+test_instances.class_attribute = :no_show
+
+puts "  Done"
+
+puts "Training the classifier"
+classifier = Weka::Classifiers::Trees::RandomForest.new
+# classifier.use_options('-I 200 -K 5')
+classifier.train_with_instances(training_instances)
+puts "  Done"
+
+
+puts "Evaluating"
+evaluation = classifier.evaluate(test_instances)
+puts "  Done"
+puts evaluation.summary
+
+
+class_attr_i = unique_feature_names.index("no_show") #training_instances.class_attribute
+
+# puts classifier.distribution_for(test_instances.first)
+answers = test_instances.collect do |instance|
+  [
+    instance.values[class_attr_i] == "true", 
+    classifier.classify(instance) == "true", 
+    classifier.distribution_for(instance)["true"]
+  ] # actual, estimated, pct
+end
+
+pct_no_show = 1.0 * answers.count {|e| e[0] } / answers.size
+
+puts "% true positive: #{ 1.0 * answers.count {|e| e[0]  and e[1] } / answers.size }"
+puts "% true negative: #{ 1.0 * answers.count {|e| !e[0] and !e[1] } / answers.size }"
+puts "% false positive: #{ 1.0 * answers.count {|e| !e[0]  and e[1] } / answers.size }"
+puts "% false negative: #{ 1.0 * answers.count {|e| e[0]  and !e[1] } / answers.size }"
+puts "% actual no show: #{ pct_no_show }"
+puts "% predicted as no show: #{ 1.0 * answers.count {|e| e[1] } / answers.size }"
+puts "rms error (classified): #{ Math.sqrt(answers.collect {|e| ((e[0] ? 1.0 : 0.0) - (e[1] ? 1.0 : 0.0)) ** 2}.mean) }"
+puts "rms error (probabilistic): #{ Math.sqrt(answers.collect {|e| ((e[0] ? 1.0 : 0.0) - e[2]) ** 2}.mean) }"
+puts "rms error if constant prob used for all: #{ Math.sqrt(answers.collect {|e| ((e[0] ? 1.0 : 0.0) - pct_no_show) ** 2}.mean) } "
 # =============================================================================
 # ==============  assign each encounter a probability of no-show  =============
 
-rb=ReportBuilder.new
-
-puts "\nMethod: Baseline performance with a constant probability for all encounters"
-Analyze.assign_odds_ratios( @encounters_all, {}, :prob_no_show)
-
-Analyze.validate_model( test_no_show, test_show, :prob_no_show)
-
-
-
-puts "\nMethod: Multiple regression"
-Analyze.assign_multiple_regression( @encounters_all, lr_model, :prob_no_show)
-
-a=@encounters_all.collect {|e| e[:prob_no_show]}.to_vector
-rb.add(Statsample::Graph::Histogram.new(a))
-
-b=all_no_show.collect {|e| e[:prob_no_show]}.to_vector
-rb.add(Statsample::Graph::Histogram.new(b))
-
-Analyze.validate_model( test_no_show, test_show, :prob_no_show)
-
-
-puts "\nMethod: Simple odds ratios"
-
-Analyze.assign_odds_ratios( @encounters_all, log_odds_ratios_by_feature_2, :prob_no_show)
-
-a=@encounters_all.collect {|e| e[:prob_no_show]}.to_vector
-rb.add(Statsample::Graph::Histogram.new(a))
-
-a=all_no_show.collect {|e| e[:prob_no_show]}.to_vector
-rb.add(Statsample::Graph::Histogram.new(a))
-
-Analyze.validate_model( test_no_show, test_show, :prob_no_show)
-
-
-
-
-rb.save_html('histogram.html')
+raise "hilo"
+# rb=ReportBuilder.new
+#
+# puts "\nMethod: Baseline performance with a constant probability for all encounters"
+# Analyze.assign_odds_ratios( @encounters_all, {}, :prob_no_show)
+#
+# Analyze.validate_model( test_no_show, test_show, :prob_no_show)
+#
+#
+#
+# puts "\nMethod: Multiple regression"
+# Analyze.assign_multiple_regression( @encounters_all, lr_model, :prob_no_show)
+#
+# a=@encounters_all.collect {|e| e[:prob_no_show]}.to_vector
+# rb.add(Statsample::Graph::Histogram.new(a))
+#
+# b=all_no_show.collect {|e| e[:prob_no_show]}.to_vector
+# rb.add(Statsample::Graph::Histogram.new(b))
+#
+# Analyze.validate_model( test_no_show, test_show, :prob_no_show)
+#
+#
+# puts "\nMethod: Simple odds ratios"
+#
+# Analyze.assign_odds_ratios( @encounters_all, log_odds_ratios_by_feature_2, :prob_no_show)
+#
+# a=@encounters_all.collect {|e| e[:prob_no_show]}.to_vector
+# rb.add(Statsample::Graph::Histogram.new(a))
+#
+# a=all_no_show.collect {|e| e[:prob_no_show]}.to_vector
+# rb.add(Statsample::Graph::Histogram.new(a))
+#
+# Analyze.validate_model( test_no_show, test_show, :prob_no_show)
+#
+#
+#
+#
+# rb.save_html('histogram.html')
 
 
 # =============================================================================
